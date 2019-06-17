935d993ebce0c33632edf4c4385441c25a9d9ea3
oomusou <oomusou@gmail.com>
2019-06-17 14:13:22 +0800

    Param with this

---
+++ src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <h2>{{ product }}</h2>
+  </div>
+</template>
+
+<script>
+let products = {
+  0: 'Shoes',
+  1: 'T-Shirts',
+  2: 'Pants',
+};
+
+let product = function() {
+  return products[this.$route.params.id] || 'N/A';
+};
+
+export default {
+  name: 'Products',
+  computed: {
+    product
+  },
+};
+</script>
